eaa16d09e44bb8966c030ce066c1afa00b0f24e8
Valeriy Osipov <info@vosipov.com>
2016-02-27 17:03:18 +0300

    Used urllib to generate URL

--- 01_Nuance_NDEV_TTS_Example.py
+++ 01_Nuance_NDEV_TTS_Example.py
@@ -3,14 +3,19 @@ import winsound
 import time
 import random
 import string
+import urllib
+import argparse
+
+# TODO: do not store parameters in file
+# TDO: PEP-8
+# TODO: commit & push
 
 # Set your TTS Text here
 ttsText = "Hello World..."
 
 # Setting TTS end-point and app creds
-appId = "NMDPTRIAL_ryan_bradon_8x8_com20150212225315"
-appKey = "572d515114aa3e0e073a0eadf8c241b5da60dd7a2ad12169ddde47b2b869140f\
-966560ab0f9eb916d8c7c4d1890586646564de8f3500f913de40c596aadaa540"
+appId = "NMDPTRIAL_emoshape20130902191817"
+appKey = "d605c70d846155dfe7397bbfa56d4b785588df2473869074db6f6cf387328d5d37546bbbb5c09a9cf9165c9e83a716d82ff5fca27b8c1757fc834a83b5d7e0ec"
 ttsUrl = "https://tts.nuancemobility.net:443/NMDPTTSCmdServlet"
 ttsEndpoint = "/tts"
 
@@ -28,14 +33,9 @@ headers = {
 }
 
 # Use Requests to send POST message with Query String
-req = requests.post(ttsUrl + ttsEndpoint +
-                    '?appId=' + appId +
-                    '&appKey=' + appKey +
-                    '&id=' + randomID +
-                    '&ttsLang=' + ttsLang +
-                    '&voice=' + voice,
-                    data=ttsText,
-                    headers=headers)
+params = urllib.urlencode({'appId': appId, 'appKey': appKey, 'id': randomID, 'ttsLang': ttsLang, 'voice': voice})
+url = '%s%s?%s' % (ttsUrl, ttsEndpoint, params)
+req = requests.post(url, data=ttsText, headers=headers)
 
 # Create wave file as Bytes (write binary mode is 'wb')
 wav_file = open('tts_' + time.strftime("%H_%M_%S") + '.wav', 'wb')
@@ -48,3 +48,12 @@ play_wav = winsound.PlaySound('tts_' + time.strftime("%H_%M_%S") +'.wav', winsou
 # Play file using Linux
 # https://pypi.python.org/pypi/pysox/0.3.6.alpha
 # http://stackoverflow.com/questions/8195544/how-to-play-wav-data-right-from-memory
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('-m', dest='message', help='your message')
+    p.add_argument('-s', dest='input_source', help='type or voice', default='type')
+    p.add_argument('-i', dest='image_file', help='The image you\'d like to label.', default='image.jpg')
+    p.add_argument("-k", dest='api_key', help='API key', required=True)
+    p.add_argument('--max-results', default=1)
+    args = p.parse_args()
