a1bb528b6bdf0c505383017d68bfa4df5c8920c2
Valeriy Osipov <info@vosipov.com>
2016-02-27 17:15:21 +0300

    PEP-8

--- 01_Nuance_NDEV_TTS_Example.py
+++ 01_Nuance_NDEV_TTS_Example.py
@@ -6,10 +6,6 @@ import string
 import urllib
 import argparse
 
-# TODO: do not store parameters in file
-# TDO: PEP-8
-# TODO: commit & push
-
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('-a', dest='app_id')
@@ -18,34 +14,36 @@ if __name__ == '__main__':
     p.add_argument('--tts_endpoint')
     args = p.parse_args()
 
-# Set your TTS Text here
-ttsText = "Hello World..."
+    # Set your TTS Text here
+    ttsText = "Hello World..."
 
-# Available Sources, http://dragonmobile.nuancemobiledeveloper.com/public/index.php?task=supportedLanguages
-ttsLang = "en_US"
-voice = "Susan"
+    # Available Sources, http://dragonmobile.nuancemobiledeveloper.com/public/index.php?task=supportedLanguages
+    ttsLang = "en_US"
+    voice = "Susan"
 
-# Set a random ID via list comprehension, randomizing a-zA-Z and digits
-randomID = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
+    # Set a random ID via list comprehension, randomizing a-zA-Z and digits
+    randomID = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
 
-# HTTP Request Headers
-headers = {
-    "Content-Type": "text/plain",
-    "Accept": "audio/x-wav"
-}
+    # HTTP Request Headers
+    headers = {
+        "Content-Type": "text/plain",
+        "Accept": "audio/x-wav"
+    }
 
-# Use Requests to send POST message with Query String
-params = urllib.urlencode({'appId': args.app_id, 'appKey': args.app_key, 'id': randomID, 'ttsLang': ttsLang, 'voice': voice})
-url = '%s%s?%s' % (args.tts_url, args.tts_endpoint, params)
-req = requests.post(url, data=ttsText, headers=headers)
+    # Use Requests to send POST message with Query String
+    params = urllib.urlencode({'appId': args.app_id, 'appKey': args.app_key, 'id': randomID, 'ttsLang': ttsLang,
+                               'voice': voice})
+    url = '%s%s?%s' % (args.tts_url, args.tts_endpoint, params)
+    req = requests.post(url, data=ttsText, headers=headers)
 
-# Create wave file as Bytes (writ binary mode is 'wb')
-with open('tts_%s' % time.strftime("%H_%M_%S") + '.wav', 'wb') as wav_file:
-    wav_file.write(req.content)
+    # Create wave file as Bytes (writ binary mode is 'wb')
+    filename = 'tts_%s.wav' % time.strftime("%H_%M_%S")
+    with open(filename, 'wb') as wav_file:
+        wav_file.write(req.content)
 
-# Play file using Windows
-play_wav = winsound.PlaySound('tts_' + time.strftime("%H_%M_%S") +'.wav', winsound.SND_FILENAME)
+    # Play file using Windows
+    play_wav = winsound.PlaySound(filename, winsound.SND_FILENAME)
 
-# Play file using Linux
-# https://pypi.python.org/pypi/pysox/0.3.6.alpha
-# http://stackoverflow.com/questions/8195544/how-to-play-wav-data-right-from-memory
+    # Play file using Linux
+    # https://pypi.python.org/pypi/pysox/0.3.6.alpha
+    # http://stackoverflow.com/questions/8195544/how-to-play-wav-data-right-from-memory
